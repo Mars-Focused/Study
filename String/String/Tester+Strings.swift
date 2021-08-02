0d3d6745dbbf4bbdf1e40acbddcf50922d4595d6
Mars <BlueLynx@live.com>
2021-08-02 15:21:19 -0400

    finished

--- String/String/Tester+Strings.swift
+++ String/String/Tester+Strings.swift
@@ -11,28 +11,40 @@ extension Tester {
     
     /// Using print command
     func printWord(word: String) {
-
+        print(word)
     }
     
     /// input "hello" should print "Hello"
     func printUpperCase(word: String) {
-
+        print(word.uppercased())
     }
     
     /// Using for loop, print each char in word
     func printLetters(word: String) {
-
+        for char in word {
+            print(char)
+        }
     }
     
     /// Using for-in-enumerated(), print odd-index chars in word
     func printOddCharsEnumerated(word: String) {
-
+        for (n, c) in word.enumerated() {
+            if n%2 == 0 {
+                print(c)
+            }
+        }
     }
     
     /// Using for-in-stride and string.index, print odd-index chars in word
     func printOddCharsStrideIndex(word: String) {
-
-    }
+        for char in stride(from: 0, to: 3, by: 2) {
+            let ans = word[word.index(word.startIndex, offsetBy: char)]
+            print(ans)
+        }
+//        print("f")
+//        print("v")
+            
+        }
     
     /**
      Pig Latin:
@@ -43,12 +55,41 @@ extension Tester {
      one -> oneyay
      */
     func printPigLatin(word: String) {
-
+        let vowels = "aeiou"
+        var newWord = word
+        let startIndex = newWord.startIndex
+        let firstCharacter = newWord[startIndex]
+        
+        if vowels.contains(firstCharacter) {
+            newWord.append("yay")
+            print(newWord)
+        } else {
+            newWord.append(firstCharacter)
+            newWord.append("ay")
+            newWord.remove(at: startIndex)
+            print(newWord)
+        }
     }
     
     /// Using loop and string.index, returns true if input is a palindrome
     func isPalindrome(word: String) -> Bool {
-
-        return false
+        let len = word.count/2
+        for i in 0..<len {
+            let start = word.index(word.startIndex, offsetBy: i)
+            let end = word.index(word.endIndex, offsetBy: (i * -1)-1)
+            
+            if word[start] != word[end] {
+                return false
+            }
+        }
+        return true
     }
+        
+//        if String(word.reversed()) == word {
+//            return true
+//        } else {
+//            return false
+//        }
+//    }
+
 }
